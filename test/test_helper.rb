9405365c58815d569ad8ebbeb0740f045f344aa6
Eloy Duran <eloy.de.enige@gmail.com>
2008-12-30 16:57:55 +0100

    Fixed spelling error.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,7 +18,7 @@ module Kernel
   private
   
   # TODO: Currently need to check for existence of the method.
-  # This is probably because the test_helper.rb file isn't required with a constsitent path from somewhere.
+  # This is probably because the test_helper.rb file isn't required with a consistent path from somewhere.
   def on_macruby?
     defined? MACRUBY_VERSION
   end unless respond_to?(:on_macruby?, true)
