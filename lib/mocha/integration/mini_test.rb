a6f6314da8bda32387da411240dc5674d756f19a
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-05 12:49:09 +0200

    Work around a MacRuby bug with remove_method and then including a mod which defines it again.

--- lib/mocha/integration/mini_test.rb
+++ lib/mocha/integration/mini_test.rb
@@ -10,10 +10,13 @@ if !MiniTest::Unit::TestCase.ancestors.include?(Mocha::API)
         
         include Mocha::API
         
-        alias_method :run_before_mocha, :run
-        remove_method :run
-        
-        include Mocha::Integration::MiniTest::Version131AndAbove
+        # For now workaround a MacRuby bug which makes it impossible to
+        # actually remove a method and include a module which defines it again.
+        #
+        # alias_method :run_before_mocha, :run
+        # remove_method :run
+        # 
+        # include Mocha::Integration::MiniTest::Version131AndAbove
         
       end
     end
--- lib/mocha/integration/mini_test/version_131_and_above.rb
+++ lib/mocha/integration/mini_test/version_131_and_above.rb
@@ -1,50 +1,94 @@
 require 'mocha/integration/mini_test/assertion_counter'
 require 'mocha/expectation_error'
 
-module Mocha
-  
-  module Integration
-    
-    module MiniTest
-      
-      def self.translate(exception)
+module MiniTest
+  class Unit
+    class TestCase
+      def self.translate_exception(exception)
         return exception unless exception.kind_of?(::Mocha::ExpectationError)
         translated_exception = ::MiniTest::Assertion.new(exception.message)
         translated_exception.set_backtrace(exception.backtrace)
         translated_exception
       end
       
-      module Version131AndAbove
-        def run runner
-          assertion_counter = AssertionCounter.new(self)
-          result = '.'
-          name = (self.respond_to?(:name) ? self.name : self.__name__)
+      def run(runner)
+        assertion_counter = ::Mocha::Integration::MiniTest::AssertionCounter.new(self)
+        result = '.'
+        name = (self.respond_to?(:name) ? self.name : self.__name__)
+        begin
           begin
-            begin
-              @passed = nil
-              self.setup
-              self.__send__ name
-              mocha_verify(assertion_counter)
-              @passed = true
-            rescue Exception => e
-              @passed = false
-              result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
-            ensure
-              begin
-                self.teardown
-              rescue Exception => e
-                result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
-              end
-            end
+            @passed = nil
+            self.setup
+            self.__send__ name
+            mocha_verify(assertion_counter)
+            @passed = true
+          rescue Exception => e
+            @passed = false
+            result = runner.puke(self.class, name, self.class.translate_exception(e))
           ensure
-            mocha_teardown
+            begin
+              self.teardown
+            rescue Exception => e
+              result = runner.puke(self.class, name, self.class.translate_exception(e))
+            end
           end
-          result
+        ensure
+          mocha_teardown
         end
+        result
       end
       
     end
-    
   end
-  
 end
+
+# For now workaround a MacRuby bug which makes it impossible to
+# actually remove a method and include a module which defines it again.
+#
+# module Mocha
+#   
+#   module Integration
+#     
+#     module MiniTest
+#       
+#       def self.translate(exception)
+#         return exception unless exception.kind_of?(::Mocha::ExpectationError)
+#         translated_exception = ::MiniTest::Assertion.new(exception.message)
+#         translated_exception.set_backtrace(exception.backtrace)
+#         translated_exception
+#       end
+#       
+#       module Version131AndAbove
+#         def run runner
+#           assertion_counter = AssertionCounter.new(self)
+#           result = '.'
+#           name = (self.respond_to?(:name) ? self.name : self.__name__)
+#           begin
+#             begin
+#               @passed = nil
+#               self.setup
+#               self.__send__ name
+#               mocha_verify(assertion_counter)
+#               @passed = true
+#             rescue Exception => e
+#               @passed = false
+#               result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
+#             ensure
+#               begin
+#                 self.teardown
+#               rescue Exception => e
+#                 result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
+#               end
+#             end
+#           ensure
+#             mocha_teardown
+#           end
+#           result
+#         end
+#       end
+#       
+#     end
+#     
+#   end
+#   
+# end
